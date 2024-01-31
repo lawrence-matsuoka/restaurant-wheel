376958620252437dcc3a429568e9822c7946b6da
lawrence-matsuoka <lawrencematsuoka@gmail.com>
2024-01-31 12:39:40 -0400

    Set constants for categories and restaurants in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Set constants for the category and restaurant wheels
+const categoryWheel = new Wheel(document.getElementById('category-wheel'), ['Burgers', 'Pizza', 'Sushi', 'Breakfast', 'Indian', '']);
+const restaurantWheel = new Wheel(document.getElementById('restaurant-wheel'), []);
+
