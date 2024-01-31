e27c08c09cd6ce1ec5e2d0b4821983efe9e2196c
lawrence-matsuoka <lawrencematsuoka@gmail.com>
2024-01-31 12:40:11 -0400

    Created spinWheel function in script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 const categoryWheel = new Wheel(document.getElementById('category-wheel'), ['Burgers', 'Pizza', 'Sushi', 'Breakfast', 'Indian', '']);
 const restaurantWheel = new Wheel(document.getElementById('restaurant-wheel'), []);
 
+// Create a function to spin the wheel
+function spinWheel() {
+    categoryWheel.spin().then(selectedCategory => {
+        const selectedRestaurants = getRestaurants(selectedCategory);
+        restaurantWheel.updateItems(selectedRestaurants).spin();
+    });
+}
+
